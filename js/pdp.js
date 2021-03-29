ce23d8ab56d67fdf40f54612a685503c05ed4e50
kelvinbiffi <kelvinbiffi@gmail.com>
2021-03-28 23:53:46 -0300

    ajustado classe paragraph para paragrafo

--- js/pdp.js
+++ js/pdp.js
@@ -16,7 +16,7 @@ var pdp = function (produtos) {
       <img class="bloco-produto__image max-full-width" src="https://kelvins.cc/curso-style-guide/${produto.id}.jpg">
       <div class="bloco-produto__info text-align-left">
         <h2 class="font-primaria h2 c-primaria">${produto.titulo}</h2>
-        <p class="bloco-produto__info__descricao font-primaria paragraph c-primaria pre-line">${produto.descricao}</p>
+        <p class="bloco-produto__info__descricao font-primaria paragrafo c-primaria pre-line">${produto.descricao}</p>
         <a class="sombra btn btn--primario text-align-center">Comprar</a>
         <a class="sombra btn btn--secundario text-align-center">Adicionar a lista de desejos</a>
       </div>
